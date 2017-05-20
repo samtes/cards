13aaeb3040aa26fdb831b4b8b8aea2a610e7c1c3
Samuel Weldemariam <weldemariam@apple.com>
2017-05-19 19:51:13 -0700

    Setup eslint

--- src/server/server.js
+++ src/server/server.js
@@ -4,3 +4,21 @@ console.log(`hello ${bleh}`);
 const obj = { hey: 1 };
 const obj2 = { ...obj, bleh: 2 };
 console.log(obj2);
+
+class AppComponent {
+  static PropTypes = {
+    blegh: "whoa"
+  };
+}
+
+switch (bleh) {
+  case 1:
+    console.log("HEY");
+    break;
+
+  case 2:
+    console.log("WHOA");
+    break;
+}
+
+console.log(<AppComponent />);
