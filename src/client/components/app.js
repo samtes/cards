09181a2126d7e103ffc059c74370eb36dc2481d2
Samuel Weldemariam <weldemariam@apple.com>
2017-05-23 23:41:31 -0700

    Add correct source-mapping

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -4,7 +4,16 @@ import React, { Component } from "react";
 
 class AppContainer extends Component {
   render () {
-    return <h1>Hello world!</h1>;
+    return (
+      <section>
+        <h1>Hello world!</h1>
+        <button onClick={this._click.bind(this)}>I am a button please click me</button>
+      </section>
+    );
+  }
+
+  _click() {
+    console.log("Clicked!!");
   }
 
   componentDidMount() {
